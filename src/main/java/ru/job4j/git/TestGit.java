ca17edf0e3c6d4ba02e587167f9353f968c74abb
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-12 20:05:32 +0600

    add new sout in TestGit

--- src/main/java/ru/job4j/git/TestGit.java
+++ src/main/java/ru/job4j/git/TestGit.java
@@ -3,5 +3,6 @@ package ru.job4j.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        System.out.println("Goodbye, Git!");
     }
 }
