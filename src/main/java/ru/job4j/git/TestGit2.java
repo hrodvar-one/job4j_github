cadc447b1939f8180253a1e690115a4532304b95
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-12 20:45:26 +0600

    edit sout in TestGit2

--- src/main/java/ru/job4j/git/TestGit2.java
+++ src/main/java/ru/job4j/git/TestGit2.java
@@ -2,6 +2,6 @@ package ru.job4j.git;
 
 public class TestGit2 {
     public static void main(String[] args) {
-        System.out.println("Bye, Git2!");
+        System.out.println("Good day, Git2!");
     }
 }
