731ea7dea987f55ba00ec2e8c1df7e942b881e5b
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-13 14:36:50 +0600

    added sout in RollbackChanges

--- src/main/java/ru/job4j/git/RollbackChanges.java
+++ src/main/java/ru/job4j/git/RollbackChanges.java
@@ -1,4 +1,9 @@
 package ru.job4j.git;
 
 public class RollbackChanges {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("Good day!");
+        System.out.println("Good night!");
+    }
 }
