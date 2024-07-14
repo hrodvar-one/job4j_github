5b6d36eda59cedc0909c4b18819dae645ae6fb9f
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-14 09:24:03 +0600

    3.4. Временное хранение. git stash. [#504832]

--- src/main/java/ru/job4j/git/TestGit.java
+++ src/main/java/ru/job4j/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Good work, Git!");
         System.out.println("Goodbye, Git!");
+        System.out.println("Good Night, Git!");
     }
 }
