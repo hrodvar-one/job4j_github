8bdd297efaf9a6586b215415ef7ccfc5c735a751
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-14 09:19:58 +0600

    add sout in TestGit2

--- src/main/java/ru/job4j/git/TestGit2.java
+++ src/main/java/ru/job4j/git/TestGit2.java
@@ -3,5 +3,6 @@ package ru.job4j.git;
 public class TestGit2 {
     public static void main(String[] args) {
         System.out.println("Good day, Git2!");
+        System.out.println("Good April, Git2!");
     }
 }
